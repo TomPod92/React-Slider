617e80b67aaea2b3c4a3c356eb18192dfa2c1e40
Tomek Podsiadlik <podsiadlik.tomek@gmail.com>
2019-05-27 12:38:18 +0200

    Updates

---
+++ precache-manifest.d52959599361849feb048a7b66e4a5bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3ef06cefbe997b27d3fe7fc6c7575c",
+    "url": "/React-Slider/index.html"
+  },
+  {
+    "revision": "dcbc9e36aa8b67abcb2d",
+    "url": "/React-Slider/static/css/main.32375186.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/React-Slider/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "dcbc9e36aa8b67abcb2d",
+    "url": "/React-Slider/static/js/main.067b87d0.chunk.js"
+  },
+  {
+    "revision": "dc9e51c4e4d124e15b1b",
+    "url": "/React-Slider/static/js/runtime~main.e12f23f5.js"
+  }
+]);
